ad4b08e70703e3393066900839edd5c899dff729
Hasmik_icml <gevorgyan.icml@gmail.com>
2021-06-09 14:18:44 +0400

    adding array4, array5

--- HW-15.js
+++ HW-15.js
@@ -1,13 +1,18 @@
 const array1 = ['a', 'b', 'c'];
 const array2 = ['d', 'e', 'f'];
 const array3 = ['d1', 'e1', 'f1'];
+const array4 = ['d1', 'e1', 'f1'];
+const array5 = ['d1', 'e1', 'f1'];
+const array6 = array1.concat(array2, array3, array4, array5);
+//  console.log(array4);
+
 
 function concat(...arg){
   let newArr = [];
   for(let element of arg){
     for (let elem of element)
-       newArr.push(el);
+       newArr.push(elem);
   }
   return newArr;
 }
-console.log(concat(array1, array2, array3));
+console.log(concat(array1, array2, array3, array4, array5));
